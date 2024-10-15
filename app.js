5c6876df8730b1505e0a48870fb8496f735dc565
lindsaybrose <lindsaybrocken@hotmail.com>
2024-10-15 12:59:21 +0100

    ticket 2 updates from feedback

--- app.js
+++ app.js
@@ -1,13 +1,8 @@
 const express = require("express");
 const app = express();
-const endpoints = require("./endpoints.json");
 const { getTopics } = require("./controllers/topics.controller");
 
 app.use(express.json());
-
-app.get("/api", (request, response) => {
-  response.status(200).send({ endpoints: endpoints });
-});
 
 app.get("/api/topics", getTopics);
 
