c83a56df5e77dd847d9096e1affb1200f0a82cbe
lindsaybrose <lindsaybrocken@hotmail.com>
2024-10-18 15:03:55 +0100

    add express

--- listen.js
+++ listen.js
@@ -1,9 +1,9 @@
 const app = require("./app");
 
-app.listen(8080, (err) => {
+app.listen(10000, (err) => {
   if (err) {
     console.log(err);
   } else {
-    console.log("Listening on 8080!");
+    console.log("Listening on 10000!");
   }
 });
